13378925488330dd8660df1d478d5aa43f7be148
artrastudiotrangnguyen-maker <artrastudio.trangnguyen@gmail.com>
2026-04-09 17:42:59 +0700

    update: favicon and site description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Mita Design | Modern & Professional Framer Websites",
-  description: "Stand out from the sea of generic AI websites. I build modern, bespoke Framer websites in weeks, not months, to help you launch faster and boost conversions.",
+  description: "Stand out from the sea of generic AI websites. I build modern, bespoke Framer websites to help you launch faster and boost conversions.",
   keywords: ["itsmita design", "Mita Design", "Framer templates", "Web Designer", "Framer Web Designer", "Creative Portfolio"],
   openGraph: {
     title: "Mita Design | Modern & Professional Framer Websites",
-    description: "Stand out from the sea of generic AI websites. I build modern, bespoke Framer websites in weeks, not months, to help you launch faster and boost conversions.",
+    description: "Stand out from the sea of generic AI websites. I build modern, bespoke Framer websites to help you launch faster and boost conversions.",
     url: "https://itsmita.design", // You should update this to your actual URL
     siteName: "Mita Design",
     locale: "en_US",
